c50a05f6ad908eeb4c51be90ceb85f9048f60d55
Ross McFarland <rwmcfa1@gmail.com>
2022-03-07 18:20:17 -0800

    Bump octodns versions to one w/root NS support

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     },
     install_requires=(
         'boto3>=1.20.26',
-        'octodns>=0.9.14',
+        'octodns>=0.9.16',
         'pycountry-convert>=0.7.2',
     ),
     license='MIT',
