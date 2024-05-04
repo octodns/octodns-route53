a83c0ee8f9e83dc09824ca3e4a88c867f215c280
James Mittermair <48706313+jmittermair@users.noreply.github.com>
2024-05-04 15:54:02 +1000

    tests: add property 'cansignhttpexchanges' to CAA
    - Added property 'cansignhttpexchanges=yes' to CAA record test.
    - Whitespace is added as this is the example provided in RFC 6844.
    - This change addresses failure to tuple unpack, caused by whitespaces
      in optional issuer parameters, when following RFC conventions.

--- tests/test_octodns_provider_route53.py
+++ tests/test_octodns_provider_route53.py
@@ -401,7 +401,7 @@ class TestRoute53Provider(TestCase):
             {
                 'ttl': 69,
                 'type': 'CAA',
-                'value': {'flags': 0, 'tag': 'issue', 'value': 'ca.unit.tests'},
+                'value': {'flags': 0, 'tag': 'issue', 'value': 'ca.unit.tests; cansignhttpexchanges=yes'},
             },
         ),
         (
@@ -1125,7 +1125,7 @@ class TestRoute53Provider(TestCase):
                 {
                     'Name': 'unit.tests.',
                     'Type': 'CAA',
-                    'ResourceRecords': [{'Value': '0 issue "ca.unit.tests"'}],
+                    'ResourceRecords': [{'Value': '0 issue "ca.unit.tests; cansignhttpexchanges=yes"'}],
                     'TTL': 69,
                 },
                 {
