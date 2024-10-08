f3738dedbd2cc698261f741baaa9fa9847f4b8da
Ross McFarland <rwmcfa1@gmail.com>
2024-10-08 11:36:21 -0700

    Bump python_requires to 3.9

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     long_description_content_type='text/markdown',
     name='octodns-route53',
     packages=find_packages(),
-    python_requires='>=3.6',
+    python_requires='>=3.9',
     tests_require=tests_require,
     url='https://github.com/octodns/octodns-route53',
     version=version(),
