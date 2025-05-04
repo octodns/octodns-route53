14e5e50cacbea566efd3d03d4bc79a0a5ecdd2a6
Ross McFarland <rwmcfa1@gmail.com>
2025-05-04 13:55:40 -0700

    Version 1.0.0 version bump & changelog update

--- octodns_route53/__init__.py
+++ octodns_route53/__init__.py
@@ -7,7 +7,7 @@ from .record import Route53AliasRecord
 from .source import Ec2Source, ElbSource
 
 # TODO: remove __VERSION__ with the next major version release
-__version__ = __VERSION__ = '0.0.7'
+__version__ = __VERSION__ = '1.0.0'
 
 # quell warnings
 Ec2Source
